24d6141959717bf874082a4d38b21f4a735e54b5
kappa0923 <tom.kappa0923@gmail.com>
2019-12-10 02:08:22 +0900

    update.

---
+++ functions/index.js
@@ -0,0 +1,28 @@
+// functions/index.js
+'use strict';
+
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+const firestore = admin.firestore();
+
+exports.trackingUploadLog = functions.region('asia-northeast1').storage.object().onFinalize((object) => {
+  const resourceState = object.resourceState;
+
+  // 削除済みの場合は処理せず終了
+  if (resourceState === 'not_exists') {
+    console.info('This is a deletion event.');
+    return 0;
+  }
+
+  return firestore.collection('logs')
+    .add(object)
+    .then(() => {
+      console.log('Write log success.');
+      return
+    })
+    .catch((error) => {
+      console.log(error);
+      throw error;
+    });
+});
